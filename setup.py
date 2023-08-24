0e628781ba9bfb033a9c13f4672807c13e94ec30
brunoiot <142072861+brunoiot@users.noreply.github.com>
2023-08-24 14:27:43 -0300

    Add setup script for cx-Freeze packaging

---
+++ setup.py
@@ -0,0 +1,18 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+build_options = {'packages': ["keyboard", "pynput"], 'excludes': []}
+
+import sys
+base = 'Win32GUI' if sys.platform=='win32' else None
+
+executables = [
+    Executable('macro.py', base=base)
+]
+
+setup(name='macro-bo2',
+      version = '1',
+      description = 'macro for bo2',
+      options = {'build_exe': build_options},
+      executables = executables)
